f7dd1c8a363182c48ba85e1fad2e113a9f8b7e91
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-06-20 11:31:17 +0100

    added some more comments to the program

--- Pling_Plang_Plong.rb
+++ Pling_Plang_Plong.rb
@@ -1,8 +1,11 @@
 def PlingPlangPlong(number)
 
+  # empty string that will be altered depending on the input
   ans = ""
 
+  #  checks the input was a number
   if(number.instance_of?(Integer) || number.instance_of?(Float))
+    # if the input is a number carry out the function
     # if it has no factors return the number as a string
     if(number % 7 != 0 && number % 5 != 0 && number % 3 != 0)
       ans = number.to_s
@@ -20,11 +23,10 @@ def PlingPlangPlong(number)
     if(number % 7 == 0)
       ans += "Plong"
     end
+  # if the input is not a number display error message
   else
       ans = "The input needs to be a number!"
   end
-
-
 
   return ans
 end
