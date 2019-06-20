f124aa44c01ac986aab5969959022810d090e002
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-06-20 10:28:25 +0100

    added in test outlines for the happy path

---
+++ spec/Pling_Plang_Plong_spec.rb
@@ -0,0 +1,31 @@
+describe "Tests for the PlingPlangPlong method" do
+
+  context "These are the tests that should pass" do
+
+    it "should return the string Pling if there is a factor of 3" do
+
+    end
+
+    it "should return the string Plang if there is a factor of 5" do
+
+    end
+
+    it "should return the string Plong if there is a factor of 7" do
+
+    end
+
+    it "should concatenate the strings Pling, Plang, Plong if there are factors of 3, 5 and 7" do
+
+    end
+
+    it "should return the number as a string if there are no factors of 3,5 or 7" do
+
+    end
+
+  end
+
+  context "These are the tests that should fail" do
+
+  end
+
+end
