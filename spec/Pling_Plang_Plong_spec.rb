ee673e2caac824c03cd228f61b30e3d952acf446
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-06-20 10:59:09 +0100

    added test for negative numbers

--- spec/Pling_Plang_Plong_spec.rb
+++ spec/Pling_Plang_Plong_spec.rb
@@ -16,10 +16,16 @@ describe "Tests for the PlingPlangPlong method" do
 
     it "should concatenate the strings Pling, Plang, Plong if there are factors of 3, 5 and 7" do
       expect(PlingPlangPlong(15)).to eq("PlingPlang")
+      expect(PlingPlangPlong(21)).to eq("PlingPlong")
+      expect(PlingPlangPlong(105)).to eq("PlingPlangPlong")
     end
 
     it "should return the number as a string if there are no factors of 3,5 or 7" do
       expect(PlingPlangPlong(8)).to eq("8")
+    end
+
+    it "should work correctly for negative numbers" do
+      expect(PlingPlangPlong(-10)).to eq("Plang")
     end
 
   end
